02cab5a7995291abe8344b281869a001aee73cd7
letty493 <lettyciadesouzasaraiva@gmail.com>
2025-04-16 12:30:29 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -3,30 +3,30 @@ create database castracao;
 create table castracao.dono(
     id int primary key not null auto_increment,
     nome varchar(100) not null,
-    cpf varchar(100) not null,
-    celular varchar(100) not null,
+    cpf varchar(11) not null,
+    celular varchar(11) not null,
     email varchar(100) not null
 );
 
 create table castracao.endereco(
     id int primary key not null auto_increment,
     rua varchar(100) not null,
-    numero varchar(100) not null,
+    numero varchar(11) not null,
     bairro varchar(100) not null,
     cidade varchar(100) not null,
     estado varchar(100) not null,
-    cep varchar(100) not null
+    cep varchar(8) not null
 );
 
 create table castracao.pet(
     id int primary key not null auto_increment,
-    nome varchar(100) not null,
-    especie varchar(100) not null,
-    idade varchar(100) not null,
-    sexo varchar(100) not null,
-    raca varchar(100) not null,
-    peso varchar(100) not null,
-    vacinado varchar(100) not null,
+    nome varchar(50) not null,
+    especie varchar(50) not null,
+    idade varchar(50) not null,
+    sexo varchar(50) not null,
+    raca varchar(50) not null,
+    peso varchar(50) not null,
+    vacinado varchar(50) not null,
     data date
 );
 
